be42e3d96787dbf24ce6a39a336779380d12ec76
ひろむ <61722475+sumiyoshihiromu@users.noreply.github.com>
2020-05-08 22:58:10 +0900

    Omikuji
    練習です。

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+
+    btn.addEventListener('click', () => {
+        const n = Math.random();
+        if (n < 0.2) {
+            btn.textContent = '大吉';
+        } else if (n < 0.4) {
+            btn.textContent = '吉';
+        } else if (n < 0.4) {
+            btn.textContent = '中吉';
+        } else {
+            btn.textContent = '凶';
+        }
+
+        
+    });
+}
